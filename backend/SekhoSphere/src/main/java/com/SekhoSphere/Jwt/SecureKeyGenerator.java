001e1a72081b2a8afbebc215f04ae30492a1b8cd
Shweta Choudhary <140094496+Shweta1015@users.noreply.github.com>
2025-06-27 20:32:33 +0530

    Update SecureKeyGenerator.java

--- backend/SekhoSphere/src/main/java/com/SekhoSphere/Jwt/SecureKeyGenerator.java
+++ backend/SekhoSphere/src/main/java/com/SekhoSphere/Jwt/SecureKeyGenerator.java
@@ -9,6 +9,5 @@ public class SecureKeyGenerator {
     public static void main(String[] args) {
         SecretKey key = Keys.secretKeyFor(io.jsonwebtoken.SignatureAlgorithm.HS512);
         String encodedKey = Base64.getEncoder().encodeToString(key.getEncoded());
-        System.out.println("Generated Secure Key: " + encodedKey);
     }
 }
